8d93e090c3c71ee6abb34aba69f4db73d89eb39c
Shahar Tefler <shahartefler@gmail.com>
2020-12-29 15:41:50 +0200

    Added wrapper to measurements calculation + added more documentation #40

--- calc_measurements.py
+++ calc_measurements.py
@@ -1,109 +1,48 @@
-import cv2 as cv
-import math_classes as geo
-import math
-
 X = 0
 Y = 1
 
 
 class VehicleMeasure:
-    camera = None
-    crosswalk = [(661, 366), (825, 679), (1010, 679), (860, 366)]
-    vehicles = None
-    fps = 0
-
     def __init__(self, camera_details, crosswalk_details):
+        """
+        Constructor for the vehicle measure class. Initialized by the camera's details and
+        the crosswalk's details.
+        :param camera_details: Details about the camera in order to calculate distance in meters.
+        :param crosswalk_details: The location of the crosswalk in the frame, and its size in meters.
+        """
         self.camera = camera_details
         self.crosswalk = crosswalk_details
+
+    def calc_distance(self, box):
+        """
+        Calculates the distance of a vehicle detected (bounding box format) to the crosswalk (4 points).
+        :param box: The bounding box of the vehicle detected.
+        :type box: list<(int, int)>
+        :return: the distance from the box to the crosswalk in meters.
+        """
+        raise NotImplementedError
+
+    def calc_velocity(self, box1, box2, duration):
+        raise NotImplementedError
+
+    def calc_acceleration(self, box1, box2, box3, duration):
+        raise NotImplementedError
 
     def get_measurements(self, box1, box2, box3):
         """
         Returns the measurements of each vehicle detected (as the Vehicle class).
         :return: Vehicle
         """
-        return self.calc_distance(box1)
+        # Calculating distances of all boxes
+        dist1 = self.calc_distance(box1)
+        dist2 = self.calc_distance(box2)
+        dist3 = self.calc_distance(box3)
 
-    def calc_distance(self, box):
-        """
-        Calculates the distance of a vehicle detected (bounding box format) to the crosswalk.
-        :param box: The bounding box of the vehicle detected.
-        :type box:
-        :return:
-        """
-        line, side = self.choose_line(box)
-        (x, y) = VehicleMeasure.choose_point(box, line, side)
-        print("POINT:")
-        print(x, y)
-        point = geo.Point(x, y)
-        # TODO: convert units from pixels to real distance based on camera calibration
-        return point.dist_from_line(line)
+        # Calculate velocity
+        velocity1_2 = self.calc_velocity(box1, box2, 1 / self.camera.fps)
+        velocity2_3 = self.calc_velocity(box2, box3, 1 / self.camera.fps)
 
-    def choose_line(self, box):
-        # Getting dimensions of the bounding box
-        (x, y) = (box[0], box[1])
-        (w, h) = (box[2], box[3])
-        print("BOX:")
-        print(x, y)
-        print(x, y+h)
-        print(x+w, h+y)
-        print(x+w, y)
+        # Calculate acceleration
+        acceleration = self.calc_acceleration(box1, box2, box3, 1 / self.camera.fps)
 
-        # Crosswalk built from 4 points - for now we'll put random numbers
-        # TODO: Add db access, and take crosswalk details from there
-
-        # Define linear lines of the crosswalk
-        cw_line1 = geo.LinearLine.gen_line_from_points(self.crosswalk[0], self.crosswalk[2])
-        print("LINE1: ")
-        print(self.crosswalk[0])
-        print(self.crosswalk[2])
-        cw_line2 = geo.LinearLine.gen_line_from_points(self.crosswalk[1], self.crosswalk[3])
-        print("LINE2: ")
-        print(self.crosswalk[1])
-        print(self.crosswalk[3])
-
-        max_x = max(self.crosswalk, key=lambda item:item[1])[0]
-        min_x = min(self.crosswalk, key=lambda item:item[1])[0]
-
-        if max_x < x:
-            print("CHOSE LINE 2")
-            return cw_line2, True
-        elif min_x > x + w:
-            print("CHOSE LINE 1")
-            return cw_line1, False
-        # Intersection 1
-        elif min_x < x < max_x:
-            print("CHOSE LINE 2")
-            return cw_line2, False
-        # Intersection 2
-        elif min_x < x + w < max_x:
-            print("CHOSE LINE 1")
-            return cw_line1, True
-
-    @staticmethod
-    def choose_point(box, line, side):
-        # Getting dimensions of the bounding box
-        (x, y) = (box[0], box[1])
-        (w, h) = (box[2], box[3])
-
-        # TODO: Add support to intersecting boxes
-        if -line.get_slope() < 0 and side:
-            return x, y
-        elif -line.get_slope() < 0 and not side:
-            return x, y + h
-        elif -line.get_slope() > 0 and not side:
-            return x + w, y
-        elif -line.get_slope() > 0 and side:
-            return x + w, y + h
-
-    def calc_velocity(self, box1, box2, duration):
-        # TODO: Get fps from capture
-        dist_diff = self.calc_distance(box2) - self.calc_distance(box1)
-        return dist_diff / (1 / self.fps)
-
-    def calc_acceleration(self, box1, box2, box3, duration):
-         # TODO: Get fps from capture
-        velocity_diff = self.calc_velocity(box2, box3) - self.calc_velocity(box1, box2)
-        return velocity_diff / (1 / self.fps)
-
-
-
+        return dist2, velocity1_2, acceleration
---
+++ kinematics_calculation.py
@@ -0,0 +1,102 @@
+import calc_measurements as measurements
+import math_classes as geo
+
+
+class KinematicsCalculation(measurements.VehicleMeasure):
+    """
+    Calculates the measurements based on kinematics.
+    """
+    def get_measurements(self, box1, box2, box3):
+        """
+        Returns the measurements of each vehicle detected (as the Vehicle class).
+        :return: Vehicle
+        """
+        d1 = self.calc_distance(box1)
+        d2 = self.calc_distance(box1)
+        d3 = self.calc_distance(box1)
+
+    def calc_distance(self, box):
+        """
+        Calculates the distance of a vehicle detected (bounding box format) to the crosswalk (4 points).
+        :param box: The bounding box of the vehicle detected.
+        :type box: list<(int, int)>
+        :return: the distance from the box to the crosswalk in meters.
+        """
+        line, side = self.choose_crosswalk_line(box)
+        (x, y) = KinematicsCalculation.choose_point(box, line, side)
+        print("POINT:")
+        print(x, y)
+        point = geo.Point(x, y)
+        # TODO: convert units from pixels to real distance
+        return point.dist_from_line(line)
+
+    def choose_crosswalk_line(self, box):
+        """
+        In order to calculate the shortest distance from the crosswalk, we need to choose the
+        closest line to the bounding box. This function calculates what line is the closest.
+        :param box: The bounding box of a vehicle detected.
+        :type box: list<(int, int)>
+        :return: The closest crosswalk line, is the crosswalk leftward to the box
+        :rtype: LinearLine object (from math_classes)
+        """
+        # Getting the dimensions of the bounding box
+        (x, y) = (box[0], box[1])
+        (width, height) = (box[2], box[3])
+
+        # TODO: Add db access, and take crosswalk details from there
+
+        # Define linear lines of the crosswalk
+        crosswalk_line1 = geo.LinearLine.gen_line_from_points(self.crosswalk[0], self.crosswalk[2])
+        crosswalk_line2 = geo.LinearLine.gen_line_from_points(self.crosswalk[1], self.crosswalk[3])
+
+        # Getting the maximal and minimal values of the crosswalk
+        crosswalk_max_x = max(self.crosswalk, key=lambda item: item[1])[0]
+        crosswalk_min_x = min(self.crosswalk, key=lambda item: item[1])[0]
+
+        '''
+        Choosing the closest line based on the locations of the box and the crosswalk
+        '''
+        # The crosswalk is leftward to the box, and there is no intersection between them.
+        if crosswalk_max_x < x:
+            return crosswalk_line2, True
+        # The crosswalk is rightward to the box, and there is no intersection between them.
+        elif crosswalk_min_x > x + width:
+            return crosswalk_line1, False
+        # The objects intersect, so that a part of the box is within the crosswalk
+        elif crosswalk_min_x < x < crosswalk_max_x:
+            return None, None
+        # The objects intersect, so that a part of the box is within the crosswalk
+        elif x < crosswalk_min_x < x + width < crosswalk_max_x:
+            print("CHOSE LINE 1")
+            return None, None
+
+    @staticmethod
+    def choose_point(box, line, is_leftward):
+        """
+        Choose the extreme point from the box according to the crosswalk line and
+        their locations.
+        :param box: The bounding box of the vehicle detected.
+        :param line: The crosswalk line closest to the box
+        :param is_leftward: Is the crosswalk leftward to the box
+        :return: The extreme point of the box
+        """
+        # Getting the dimensions of the bounding box
+        (x, y) = (box[0], box[1])
+        (width, height) = (box[2], box[3])
+
+        if line.get_slope() > 0 and is_leftward:
+            return x, y
+        elif line.get_slope() > 0 and not is_leftward:
+            return x, y + height
+        elif line.get_slope() < 0 and not is_leftward:
+            return x + width, y
+        elif line.get_slope() < 0 and is_leftward:
+            return x + width, y + height
+
+    def calc_velocity(self, box1, box2, duration):
+        dist_diff = self.calc_distance(box2) - self.calc_distance(box1)
+        return dist_diff / (1 / self.camera_details.fps)
+
+    def calc_acceleration(self, box1, box2, box3, duration):
+        velocity_diff = self.calc_velocity(box2, box3) - self.calc_velocity(box1, box2)
+        return velocity_diff / (1 / self.fps)
