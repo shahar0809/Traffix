2c6288b2a19409aa2e552ba595744c9ee84246b3
yaara hazan <yaarahazan@gmail.com>
2020-12-29 14:27:04 +0200

    issue #42

--- DB Wrapper.py
+++ DB Wrapper.py
@@ -1,10 +1,11 @@
 import sqlite3
 import datetime as dt
+from abc import ABC
 
 MAX_DAY = 7
 
 
-class SqliteDatabase:
+class IDatabase:
     """
 
     """
@@ -20,10 +21,7 @@ class SqliteDatabase:
     def get_environment(self, env_id):
         raise NotImplementedError
 
-    def get_traffic_data(self, day, hour):
-        raise NotImplementedError
-
-    def set_camera_details(self, focal_length, optical_center, camera_id):
+    def set_camera_details(self, focal_length, optical_center):
         raise NotImplementedError
 
     def set_crosswalk_details(self, crosswalk_point, env_id):
@@ -32,9 +30,14 @@ class SqliteDatabase:
     def set_traffic_bars(self, env_id, traffic_bar):
         raise NotImplementedError
 
+    def get_traffic_data(self, day, hour):
+        raise NotImplementedError
+
     def set_traffic_per_week(self, loads_list):
         raise NotImplementedError
 
+
+class SqliteDatabase(IDatabase):
     def create_connection(self, db_file):
         """
         create a database connection to the SQLite database specified by db_file
@@ -51,32 +54,32 @@ class SqliteDatabase:
 
     def sql_table(self, db_file):
         sql_create_cameras_table = """CREATE TABLE IF NOT EXISTS cameras (
-                                            id integer PRIMARY KEY,
-                                            focal_length integer NOT NULL,
-                                            optical_center_x integer NOY NULL,
-                                            optical_center_y integer NOY NULL
-                                        );"""
+                                                id integer PRIMARY KEY,
+                                                focal_length integer NOT NULL,
+                                                optical_center_x integer NOY NULL,
+                                                optical_center_y integer NOY NULL
+                                            );"""
 
         sql_create_environments_table = """ CREATE TABLE IF NOT EXISTS environments (
-                                               id integer PRIMARY KEY,
-                                               camera_id integer NOT NULL,
-                                               crosswalk_point_1 point NOT NULL,
-                                               crosswalk_point_2 point NOT NULL,
-                                               crosswalk_point_3 point NOT NULL,
-                                               crosswalk_point_4 point NOT NULL,
-                                               traffic_bar_low integer NOT NULL,
-                                               traffic_bar_mid integer NOT NULL,
-                                               traffic_bar_high integer NOT NULL,
-                                               FOREIGN KEY (camera_id) REFERENCES cameras (id)
-                                           ); """
+                                                   id integer PRIMARY KEY,
+                                                   camera_id integer NOT NULL,
+                                                   crosswalk_point_1 point NOT NULL,
+                                                   crosswalk_point_2 point NOT NULL,
+                                                   crosswalk_point_3 point NOT NULL,
+                                                   crosswalk_point_4 point NOT NULL,
+                                                   traffic_bar_low integer NOT NULL,
+                                                   traffic_bar_mid integer NOT NULL,
+                                                   traffic_bar_high integer NOT NULL,
+                                                   FOREIGN KEY (camera_id) REFERENCES cameras (id)
+                                               ); """
 
         sql_create_loads_table = """ CREATE TABLE IF NOT EXISTS loads (
-                                                   env_id integer PRIMARY KEY,
-                                                   priority ENUM('Low', 'Medium', 'High') NOT NULL,
-                                                   hour integer NOT NULL,
-                                                   day integer NOT NULL,
-                                                   FOREIGN KEY (env_id) REFERENCES environments (id)
-                                               ); """
+                                                       env_id integer PRIMARY KEY,
+                                                       priority ENUM NOT NULL,
+                                                       hour integer NOT NULL,
+                                                       day integer NOT NULL,
+                                                       FOREIGN KEY (env_id) REFERENCES environments (id)
+                                                   ); """
 
         # create a database connection
         self.conn = self.create_connection(db_file)
@@ -106,10 +109,7 @@ class SqliteDatabase:
             c = self.conn.cursor()
             c.execute(create_table_sql)
         except sqlite3.Error as e:
-            print(e)
-
-
-class IDatabase(SqliteDatabase):
+            return e
 
     def get_camera_details(self, camera_id):
         """
@@ -118,11 +118,15 @@ class IDatabase(SqliteDatabase):
         :return: camera details
         """
         #select camera details
-        sql_select = """SELECT * FROM cameras WHERE ID = ?"""
+        sql_select = "SELECT * FROM cameras WHERE ID = ?"
         cursor = self.conn.execute(sql_select, (camera_id,))
-        return_select = cursor.fetchone()
+        record = cursor.fetchall()
 
-        return return_select
+        for row in record:
+            print("id = ", row[0])
+            print("focal_length = ", row[1])
+            print("optical_center_x = ", row[2])
+            print("optical_center_y  = ", row[3])
 
     def get_crosswalk_details(self, env_id):
         """
@@ -133,54 +137,52 @@ class IDatabase(SqliteDatabase):
         #select crosswalk details
         sql_select = "SELECT Crosswalk_point_1, Crosswalk_point_2, Crosswalk_point_3, Crosswalk_point_4 FROM environments WHERE ID = ?"
         cursor = self.conn.execute(sql_select, (env_id,))
-        return_select = cursor.fetchone()
+        record = cursor.fetchall()
 
-        return return_select
+        for row in record:
+            print("Crosswalk_point_1 = ", row[0])
+            print("Crosswalk_point_2 = ", row[1])
+            print("Crosswalk_point_3 = ", row[2])
+            print("Crosswalk_point_4  = ", row[3])
 
     def get_environment(self, env_id):
         """
            return environments details
-           :param **kwargs:
            :param env_id: The id of the environments that we want to return
            :return: environments details
            """
         # select environments details
         sql_select = """SELECT * FROM environments WHERE ID = ?"""
         cursor = self.conn.execute(sql_select, (env_id,))
-        return_select = cursor.fetchone()
+        record = cursor.fetchall()
 
-        return return_select
+        for row in record:
+            print("camera_id integer ", row[0])
+            print("crosswalk_point_1 = ", row[1])
+            print("Crosswalk_point_2 = ", row[2])
+            print("Crosswalk_point_3 = ", row[3])
+            print("Crosswalk_point_4  = ", row[4])
+            print("traffic_bar_low  = ", row[5])
+            print("traffic_bar_mid  = ", row[6])
+            print("traffic_bar_high  = ", row[7])
 
-    def get_traffic_data(self, day, hour):
-        """
-        return traffic data
-        :param day: Will indicate a relevant environment
-        :param hour: Will indicate a relevant environment
-        :return: crosswalk details
-        """
-        # select crosswalk details
-        sql_select = "SELECT level FROM loads WHERE day = %d AND hour = %d"
-        cursor = self.conn.execute(sql_select, (day, hour))
-        return_select = cursor.fetchone()
-
-        return return_select
-
-    def set_camera_details(self, focal_length, optical_center, camera_id):
+    def set_camera_details(self, focal_length, optical_center):
         """
            insert to the table cameras details
            :param focal_length: The focal length that we want to update to.
            :param  optical_center: A list of the information we want to update to.
-           :param  camera_id: The place where we want to update.
            :return: true if the update works, false if doesn't.
            """
         cursor = self.conn.cursor()
-        # update camera details
-        sql_update = "UPDATE cameras SET focal_length= %d, optical_center_x = %d, optical_center_y = %d WHERE camera_id = %d"
-
+        # insert camera details
+        sql_insert = "INSERT INTO cameras (focal_length, optical_center_x, optical_center_y)" \
+                     "VALUES (?, ?, ?)"
+        val = (focal_length, optical_center[0], optical_center[1])
         try:
-            insert = cursor.execute(sql_update, (focal_length, optical_center[0], optical_center[1], camera_id))
+            cursor.execute(sql_insert, val)
             self.conn.commit()
             cursor.close()
+            print("The details inserts")
             return True
 
         except sqlite3.Error as e:
@@ -196,17 +198,31 @@ class IDatabase(SqliteDatabase):
         """
         cursor = self.conn.cursor()
         # update crosswalk details
-        sql_update = "UPDATE environments SET crosswalk_point_1 = %d, crosswalk_point_2 = %d, crosswalk_point_3 = %d, crosswalk_point_4 = %d WHERE env_id = %d"
+        sql_update = "UPDATE environments SET crosswalk_point_1 = ?, crosswalk_point_2 = ?, crosswalk_point_3 = ?, crosswalk_point_4 = ? WHERE env_id = ?"
 
         try:
             cursor.execute(sql_update, (crosswalk_point[0], crosswalk_point[1], crosswalk_point[2], crosswalk_point[3], env_id))
             self.conn.commit()
             cursor.close()
-            return True
+            return "The details inserts"
 
         except sqlite3.Error as e:
             cursor.close()
             return e
+
+    def get_traffic_data(self, day, hour):
+        """
+        return traffic data
+        :param day: Will indicate a relevant environment
+        :param hour: Will indicate a relevant environment
+        :return: crosswalk details
+        """
+        # select crosswalk details
+        sql_select = "SELECT level FROM loads WHERE day = %d AND hour = %d"
+        cursor = self.conn.execute(sql_select, (day, hour))
+        return_select = cursor.fetchone()
+
+        return return_select
 
     def set_traffic_bars(self, env_id, traffic_bar):
         """
@@ -229,33 +245,6 @@ class IDatabase(SqliteDatabase):
             cursor.close()
             return e
 
-    def set_traffic_per_week(self, hours):
-        """
-        delete old data and insert new.
-        :param hours:
-        :return: environments details
-        """
-        # delete loads data
-        sql_delete = "DELETE FROM loads"
-        cursor = self.conn.execute(sql_delete)
-        d = cursor.fetchone()
-
-        m = 1
-        n = 0
-        try:
-            for i in range(1, 8):
-                while n < 24:
-                    self.get_traffic_data(m, hours[n])
-                    n += 1
-                m += 1
-                n = 0
-
-            return True
-
-        except sqlite3.Error as e:
-            cursor.close()
-            return e
-
 
 def main():
     database = r"C:/Users/משתמש/traffix/traffixDB.db"
@@ -264,6 +253,24 @@ def main():
 
     sqlite_database.create_connection(database)
     sqlite_database.sql_table(database)
+    try:
+        focal_length = int(input("Enter the focal_length you want to insert to the cameras table: "))
+        optical_center_x = int(input("Enter the optical_center_x you want to insert to the cameras table: "))
+        optical_center_y = int(input("Enter the optical_center_y you want to insert to the cameras table: "))
+        optical_center = [optical_center_x, optical_center_y]
+        sqlite_database.set_camera_details(focal_length, optical_center)
+
+        camera_id = int(input("\nEnter the ID of the camera from which you want details: "))
+        sqlite_database.get_camera_details(camera_id)
+
+        env_id = int(input("\nEnter the ID of the environment from which you want details: "))
+        print(sqlite_database.get_environment(env_id))
+
+        env_id = int(input("\nEnter the ID of the environment from which you want the crosswalk details: "))
+        print(sqlite_database.get_crosswalk_details(env_id))
+
+    except Exception as e:
+        print(e)
 
 
 if __name__ == '__main__':
