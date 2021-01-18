cc6e81e9093471d22f122412d3baaaa7719f5197
yaara hazan <yaarahazan@gmail.com>
2021-01-18 18:13:25 +0200

    edd environment to the decision

--- decision_making.py
+++ decision_making.py
@@ -1,8 +1,22 @@
 import kinematics_calculation as kinematics
 import cmath
+import DB_Wrapper as db
+import weather_wrapper as weather
+
+crosswalk = 0
+m = kinematics.KinematicsCalculation(None, crosswalk)
+
+HIGH_LEVEL = 3
+MID_LEVEL = 2
+LOW_LEVEL = 1
 
 
 class Decision:
+    def get_traffic_level_from_database(self):
+        raise NotImplementedError
+
+    def calculate_time(self, distance, velocity, acceleration):
+        raise NotImplementedError
 
     def make_decision(self, box, box1, box2, box3, duration, m):
         raise NotImplementedError
@@ -10,6 +24,8 @@ class Decision:
     def make_decision_for_vehicle(self, vehicle):
         raise NotImplementedError
 
+    def get_fps_from_database(self):
+        
 
 class DecisionMaker(Decision):
     def __init__(self, vehicles, traffic_level, weather):
@@ -17,12 +33,17 @@ class DecisionMaker(Decision):
         self.traffic_level = traffic_level
         self.weather = weather
 
-    def make_decision(self, box, box1, box2, box3, duration, m):
+    def get_traffic_level_from_database(self):
+        database = db.SqliteDatabase()
+        try:
+            day = int(input("Please enter a day from which you want the traffic level"))
+            hour = int(input("Please enter a day from which you want the traffic level"))
+            return database.get_traffic_data(day, hour)
 
-        velocity = m.calc_velocity(box1, box2, duration)
-        acceleration = m.calc_acceleration(box1, box2, box3, duration)
-        distance = m.calc_distance(box)
+        except Exception as e:
+            print(e)
 
+    def calculate_time(self, distance, velocity, acceleration):
         a = acceleration / 2
         b = velocity
         c = distance
@@ -32,50 +53,34 @@ class DecisionMaker(Decision):
 
         if calc_dis < 0 or a == 0:
             return "Impossible calculator"
-
         else:
             d = cmath.sqrt(calc_dis)
             x1 = (-b + d) / (2 * a)
             x2 = (-b - d) / (2 * a)
-            if x1.real < float(180) or x2.real < float(180):
-                return "Vehicles stopped. Pedestrians keep walking"
+            return x1.real, x2.real
 
-            else:
-                return "Pedestrians stopped. Vehicles keep driving"
+    def make_decision(self, box, box1, box2, box3, duration, m):
+        decision = Decision()
+
+        distance = m.calc_distance(box)
+        velocity = m.calc_velocity(box1, box2, duration)
+        acceleration = m.calc_acceleration(box1, box2, box3, duration)
+
+        calc = decision.calculate_time(distance, velocity, acceleration)
+        if calc[0] < 1 or calc[1] < 1:
+            return ""
+        elif calc[0] < 20 or calc[1] < 20:
+            return "Vehicles stopped. Pedestrians keep walking"
+        else:
+            return "Pedestrians stopped. Vehicles keep driving"
 
     def make_decision_for_vehicle(self, vehicle):
-        a = vehicle.acceleration / 2
-        b = vehicle.velocity
-        c = vehicle.distance
+        decision = Decision()
 
-        # calculate the discriminant
-        calc_dis = (b ** 2) - (4 * a * c)
-
-        if calc_dis < 0 or a == 0:
-            return "Impossible calculator"
-
+        calc = decision.calculate_time(vehicle.distance, vehicle.velocity, vehicle.acceleration)
+        if calc[0] < 20 or calc[1] < 20:
+            return "Vehicles stopped. Pedestrians keep walking"
         else:
-            d = cmath.sqrt(calc_dis)
-            x1 = (-b + d) / (2 * a)
-            x2 = (-b - d) / (2 * a)
-            if x1.real < float(180) or x2.real < float(180):
-                return "Vehicles stopped. Pedestrians keep walking"
-
-            else:
-                return "Pedestrians stopped. Vehicles keep driving"
+            return "Pedestrians stopped. Vehicles keep driving"
 
 
-def main():
-    decision_maker = DecisionMaker()
-    m = kinematics.KinematicsCalculation(None, [(2, 60), (60, 60), (80, 2), (1, 80)])
-    box = [(1, 60), (60, 60), (80, 2), (1, 80)]
-    box1 = [(1, 60), (60, 60), (80, 2), (1, 80)]
-    box2 = [(1, 60), (80, 60), (1, 1), (1, 80)]
-    box3 = [(1, 60), (80, 60), (1, 1), (1, 80)]
-    duration = 1 / 0.2456
-
-    decision_maker.make_decision(box, box1, box2, box3, duration, m)
-
-
-if __name__ == '__main__':
-    main()
--- weather_wrapper.py
+++ weather_wrapper.py
@@ -7,11 +7,13 @@ LATITUDE = 0
 LONGITUDE = 1
 RESP = '{"coord":{"lon":34.7806,"lat":32.0809},"weather":[{"id":800,"main":"Clear","description":"clear sky","icon":"01d"}],"base":"stations","main":{"temp":23,"feels_like":21.74,"temp_min":21.11,"temp_max":24,"pressure":1018,"humidity":41},"visibility":10000,"wind":{"speed":1.5,"deg":0},"clouds":{"all":0},"dt":1610019026,"sys":{"type":1,"id":6845,"country":"IL","sunrise":1609994540,"sunset":1610031094},"timezone":7200,"id":293397,"name":"Tel Aviv","cod":200}'
 
+
 def get_current_utc_timestamp():
     current_datetime = datetime.datetime.utcnow()
     current_timetuple = current_datetime.utctimetuple()
     current_timestamp = calendar.timegm(current_timetuple)
     return current_timestamp
+
 
 class WeatherAPI:
     API_URL = "http://api.openweathermap.org/data/2.5/weather?"
