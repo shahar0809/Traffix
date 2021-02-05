aff042dd2b458358d6491e47301ada6f990a6258
yaara_16 <yaarahazan@gmail.com>
2021-02-05 10:14:24 +0200

    delete file

--- measurements_calculations/v.py
+++
@@ -1,8 +0,0 @@
-def to_string(crosswalk_points):
-    str = ''.join(crosswalk_points)
-    x = str(crosswalk_points[0])
-    y = str(crosswalk_points[1])
-    (x, y) = (x, y)
-    print(str)
-
-to_string((5, 6))
