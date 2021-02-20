52879fdde66e0a0f3505ad249f1fdfb139266429
Shahar Tefler <shahartefler@gmail.com>
2021-02-20 22:16:45 +0200

    Add a container to all windows and a multiple-window OOP structure #103

--- gui/traffix_gui.py
+++ gui/traffix_gui.py
@@ -1,4 +1,39 @@
 import tkinter as tk
+from gui import choose_location, choose_camera, environment_stream, \
+    home, mark_crosswalk, new_camera, new_environment, set_traffic_bars, update_environment
 
-class TraffixGUI:
-    pass
+windows = {
+    "CHOOSE_CAMERA": choose_camera.ChooseCamera,
+    "CHOOSE_LOCATION": choose_location.ChooseLocation,
+    "ENV_STREAM": environment_stream.EnvironmentStream,
+    "HOME": home.Home,
+    "MARK_CROSSWALK": mark_crosswalk.MarkCrosswalk,
+    "NEW_CAMERA": new_camera.NewCamera,
+    "NEW_ENV": new_environment.NewEnvironment,
+    "TRAFFIC_BARS": set_traffic_bars.SetTrafficBars,
+    "UPDATE_ENV": update_environment.UpdateEnvironment
+}
+
+
+class TraffixGUI(tk.Tk):
+    def __init__(self, *args, **kwargs):
+        tk.Tk.__init__(self, *args, **kwargs)
+        container = tk.Frame(self)
+
+        container.pack(side="top", fill="both", expand=True)
+
+        container.grid_rowconfigure(0, weight=1)
+        container.grid_columnconfigure(0, weight=1)
+
+        self.frames = {}
+
+        for window_name in windows.keys():
+            frame = windows[window_name](container, self)
+            self.frames[windows[window_name]] = frame
+            frame.grid(row=0, column=0, sticky="nsew")
+
+        self.show_frame(windows["HOME"])
+
+    def show_frame(self, cont):
+        frame = self.frames[cont]
+        frame.tkraise()
