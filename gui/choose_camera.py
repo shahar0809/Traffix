7682cd02dab8ec1f9578a63de827e7b9d3e8c3d0
yaara hazan <yaarahazan@gmail.com>
2021-02-25 11:44:27 +0200

    finish to work on issue #93 and almost finish issue #95

--- gui/choose_camera.py
+++ gui/choose_camera.py
@@ -1,8 +1,19 @@
 import tkinter as tk
 import gui.screen as screen
+from gui import new_camera
+
 
 class ChooseCamera(screen.Screen):
     def __init__(self, parent, controller):
         super().__init__(parent, controller)
+        nums = 3
 
-        # Continue adding elements here...
+        # for num in range(nums):
+        # tk.Button(self, text="show camera", command=self).pack()
+
+        tk.Button(self, text="add camera", command=self.open_new_camera).pack()
+
+    def open_new_camera(self):
+        self.controller.show_frame(new_camera.NewCamera)
+        self.destroy()
+
