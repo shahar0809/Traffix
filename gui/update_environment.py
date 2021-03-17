ec45ccef476d9ae78830be88d8e7267678ba84e6
Shahar Tefler <shahartefler@gmail.com>
2021-03-17 21:06:06 +0200

    Revert "Fix crosswalk mark - works perfectlyyy #97"
    This reverts commit 85cc2e638e4e419c8a7d85f46cca6ade4c83e25e.

--- gui/update_environment.py
+++
@@ -1,9 +0,0 @@
-import tkinter as tk
-import gui.screen as screen
-
-
-class UpdateEnvironment(screen.Screen):
-    def __init__(self, parent, controller):
-        super().__init__(parent, controller)
-
-        # Continue adding elements here...
